0cc7a2c60032dbc3276818cadad86855340c34ab
efrem <eopiscool@gmail.com>
2023-03-20 23:26:05 -0700

    Updated createStore function to configureStore via @reactjs/toolkit since createStore was deprecated

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -1,10 +1,11 @@
 import { createStore, applyMiddleware } from 'redux';
+import { configureStore } from '@reduxjs/toolkit';
 import logger from 'redux-logger';
 import thunk from 'redux-thunk'
 import rootReducer from '../reducers/root_reducer';
 
-const configureStore = (preloadedState = {}) => {
-  return createStore(rootReducer, preloadedState, applyMiddleware(thunk, logger));
+const myconfigureStore = (preloadedState = {}) => {
+  return configureStore({ reducer: rootReducer, middleware: [thunk, logger], preloadedState});
 };
 
-export default configureStore;
+export default myconfigureStore;
