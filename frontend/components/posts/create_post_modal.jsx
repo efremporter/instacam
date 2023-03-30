3e88c4e1c3b1c5a1dca8aa73cd391b4dc07085dd
efrem <eopiscool@gmail.com>
2023-03-30 16:46:37 -0700

    Can successfully upload file into createPostModal

--- frontend/components/posts/create_post_modal.jsx
+++ frontend/components/posts/create_post_modal.jsx
@@ -1,8 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { IoImagesOutline } from 'react-icons/io5'
 
 function CreatePostModal() {
+  const [images, setImages] = useState([]);
 
+  function handleImage(e) {
+    images.push(...e.target.files);
+    setImages(images);
+    // Line below clears the input field after 
+    document.getElementById("upload-image-input").value = "";
+  }
+  
   return (
     <div className='create-post-modal-container'>
       <div className='create-post-modal-header'>
@@ -13,7 +21,10 @@ function CreatePostModal() {
         <div className='create-post-modal-body'>
           <IoImagesOutline size={65} color="white"/>
           <div className='create-post-modal-message'>Drag photos here to upload</div>
-          <button className='create-post-modal-button'>Select from computer</button>
+          <label className='create-post-modal-button'>
+             Select from computer
+            <input id="upload-image-input" type="file" accept="image/*" onChange={handleImage}/>
+          </label>
         </div>
       </div>
     </div>
