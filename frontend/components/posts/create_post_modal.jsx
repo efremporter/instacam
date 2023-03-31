b2295056e1bf19edd4aa134a48c0d807242fd11b
efrem <eopiscool@gmail.com>
2023-03-30 19:10:04 -0700

    createPostModal first page is responsive

--- frontend/components/posts/create_post_modal.jsx
+++ frontend/components/posts/create_post_modal.jsx
@@ -3,49 +3,56 @@ import { IoImagesOutline } from 'react-icons/io5'
 
 function CreatePostModal() {
   const [images, setImages] = useState([]);
+  let uploaded = images.length > 0;
 
   function handleImage(e) {
-    const files = Array.from(e.target.files);
-    if (images.length + files.length > 10) {
+    const uploadedFiles = Array.from(e.target.files);
+    if (uploadedFiles.length < 1) return;
+    if (images.length + uploadedFiles.length > 10) {
       // Instagram only allows 10 photos per post at this time
-      const overflow = 10 - (images.length + files.length);
+      const overflow = 10 - (images.length + uploadedFiles.length);
       if (overflow > 0) {
-        images.push(...files.slice(0, overflow));
+        images.push(...uploadedFiles.slice(0, overflow));
         setImages(images);
       };
-      console.log(`Some files were not uploaded. You can only choose 10 or fewer files.`)
+      console.log("Some files were not uploaded. You can only choose 10 or fewer files.")
     } else {
-      images.push(...files);
+      images.push(...uploadedFiles);
       setImages(images);
     };
     // Line below clears the input field after 
     document.getElementById("upload-image-input").value = "";
+
   }
   
-  return (
+  const content = uploaded ? 1 : (
     <div className='create-post-modal-container'>
       <div className='create-post-modal-header'>
         Create new post
       </div>
       <div className='create-post-modal-divider'></div>
-      <div className='create-post-modal-body-container'>
-        <div className='create-post-modal-body'>
-          <IoImagesOutline size={65} color="white"/>
-          <div className='create-post-modal-message'>Drag photos here to upload</div>
-          <label className='create-post-modal-button'>
-             Select from computer
-            <input 
-              id="upload-image-input"
-              type="file"
-              accept="image/*"
-              multiple="multiple"
-              limit="2"
-              onChange={handleImage}/>
-          </label>
+      <div className='create-post-modal-body-outer-container'>
+        <div className='create-post-modal-body-inner-container'>
+          <div className='create-post-modal-body'>
+            <IoImagesOutline size={65} color="white" />
+            <div className='create-post-modal-message'>Drag photos here to upload</div>
+            <label className='create-post-modal-button'>
+              Select from computer
+              <input
+                id="upload-image-input"
+                type="file"
+                accept="image/*"
+                multiple="multiple"
+                limit="2"
+                onChange={handleImage} />
+            </label>
+          </div>
         </div>
       </div>
     </div>
   );
+  
+  return content;
 };
 
 export default CreatePostModal;
