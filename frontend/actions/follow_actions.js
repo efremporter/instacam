d0ec32f81de46565c7fa6d8c87a83448f5060101
efrem <eopiscool@gmail.com>
2023-05-22 15:35:44 -0700

    FollowActions done

---
+++ frontend/actions/follow_actions.js
@@ -0,0 +1,46 @@
+import * as FollowApiUtil from '../utils/follow_util';
+
+export const RECEIVE_FOLLOW = 'RECEIVE_FOLLOW';
+export const RECEIVE_FOLLOWS = 'RECEIVE_FOLLOWS';
+export const REMOVE_FOLLOW = 'REMOVE_FOLLOW';
+
+const receiveFollows = follows => {
+  return {
+    type: RECEIVE_FOLLOW,
+    data: follows
+  };
+};
+
+const receiveFollow = follow => {
+  return {
+    type: RECEIVE_FOLLOW,
+    data: follow
+  };
+};
+
+const removeFollow = id => {
+  return {
+    type: REMOVE_FOLLOW,
+    data: id
+  };
+};
+
+export const fetchFollows = (userId) => dispatch => {
+  return FollowApiUtil.fetchFollows(userId)
+  .then(follows => dispatch(receiveFollows(follows)));
+};
+
+export const fetchFollow = (userId, followingId) => dispatch => {
+  return FollowApiUtil.fetchFollow(userId, followingId)
+  .then(follow => dispatch(receiveFollow(follow)));
+};
+
+export const createFollow = (userId, followingId) => dispatch => {
+  return FollowApiUtil.createFollow(userId, followingId)
+  .then(follow => dispatch(receiveFollow(follow)));
+};
+
+export const deleteFollow = (id, followId) => dispatch => {
+  return FollowApiUtil.deleteFollow(id)
+  .then(() => dispatch(removeFollow(followId)));
+};
