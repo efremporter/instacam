87dce9934fb6377848e7d2f955137e72c791f4bc
efrem <eopiscool@gmail.com>
2023-06-14 17:46:01 -0700

    Fixed empty content button on feed

--- frontend/components/comments/feed_comments.jsx
+++ frontend/components/comments/feed_comments.jsx
@@ -48,6 +48,7 @@ useEffect(() => {
   };
 
   const postComment = () => {
+    if (!content.trim().length) return;
     if (content.length <= 2200) {
       const comment = { post_id: postId, user_id: currentUserId, content }
       createComment(comment)
@@ -67,6 +68,11 @@ useEffect(() => {
       from: "feed"
     };
     openModal(modal);
+  };
+
+  const checkForContent = () => {
+    if (!content.trim().length) return 'add-comment-content-empty';
+    return null;
   };
 
   return (
@@ -116,7 +122,7 @@ useEffect(() => {
           type="submit"
           onChange={e => updateCommentContent(e.target.value)}
         />
-        <div id="post-comment-button" className="add-a-comment-post-button"
+        <div id={checkForContent()} className="add-a-comment-post-button"
           onClick={postComment}
         >Post</div>
       </div>
